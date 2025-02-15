9de8ad5257871122ae344146f6f6f9768d81dc5f
nathan dardalhon <nathqn.dardalhon@gmail.com>
2025-02-15 09:01:34 +0100

    oui

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.heart').addEventListener('click', function() {
+    const hiddenMessage = document.querySelector('.hidden-message');
+    hiddenMessage.style.display = 'block';
+}); 
